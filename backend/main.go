2cb03224a27ab629f74e0e5f48d457bfffa6c48e
DenisKantic <denis.kantic18@gmail.com>
2024-07-23 00:13:08 +0200

    created half function for imagesUpload on goolang. Pushing so I can continue later

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 import "backend/uploadImages"
 
 func main() {
 	fmt.Println(uploadImages.TestingFunction())
 	fmt.Println("test again")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/uploadImages", uploadImages.TestingFunction())
+	fmt.Println("Server is running on 8080 port")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- backend/uploadImages/main.go
+++ backend/uploadImages/main.go
@@ -1,9 +1,46 @@
 package uploadImages
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
-func TestingFunction() string {
-	result := fmt.Sprintf("IMAGES TEST")
+func TestingFunction(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	return result
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	err := r.ParseMultipartForm(100 << 20) // 100 MB max
+	if err != nil {
+		http.Error(w, "Error processing form", http.StatusInternalServerError)
+		return
+	}
+
+	files := r.MultipartForm.File["images"]
+
+	for _, file := range files {
+		// open the uploaded file
+		src, err := file.Open()
+		if err != nil {
+			http.Error(w, "Error opening the file", http.StatusInternalServerError)
+			return
+		}
+
+		defer src.Close()
+
+		// create a new file in the server
+		dst, err := os.Create(filepath.Join("uploads", file.Filename))
+		if err != nil {
+			http.Error(w, "Error creating a new file in the server", http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		// copy the uploaded file
+	}
 }
