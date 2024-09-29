e7da8b4987d15580f2ba80fd1a67295acaf42963
DenisKantic <denis.kantic18@gmail.com>
2024-09-29 22:04:14 +0200

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from './AuthContext'
+import Footer from './home/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} id="__next">
         <AuthProvider>{children}</AuthProvider>
+        <Footer />
       </body>
     </html>
   )
