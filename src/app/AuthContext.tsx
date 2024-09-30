88108f717c25f1fe7cc8522d8300964c00319c42
DenisKantic <denis.kantic18@gmail.com>
2024-09-30 18:41:03 +0200

    url fix

--- src/app/AuthContext.tsx
+++ src/app/AuthContext.tsx
@@ -38,12 +38,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const register = async (formData: FormData) => {
     try {
-      const response = await axios.post('http://api/register', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        withCredentials: true,
-      })
+      const response = await axios.post(
+        'https://www.zenska-vizija.ba/api/register',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          withCredentials: true,
+        }
+      )
       return response.status
     } catch (error: any) {
       console.log('error registering user', error)
@@ -55,12 +59,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
     console.log('SENT FORM DATA', formData)
 
     try {
-      const response = await axios.post('http://api/login', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        withCredentials: true,
-      })
+      const response = await axios.post(
+        'https://www.zenska-vizija.ba/api/login',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          withCredentials: true,
+        }
+      )
 
       if (response.status === 200) {
         setIsAuthenticated(true)
@@ -77,7 +85,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const logout = async () => {
     try {
-      await axios.post('http://api/logout', null, {
+      await axios.post('https://www.zenska-vizija.ba/api/logout', null, {
         withCredentials: true,
       })
       setIsAuthenticated(false)
